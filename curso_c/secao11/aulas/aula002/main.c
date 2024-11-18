692020be42ac2a080c841d717ce1a724a2ed46de
vinic <viniciustegi1235@gmail.com>
2024-11-18 10:35:17 -0300

    commit again

--- curso_c/secao11/aulas/aula002/main.c
+++ curso_c/secao11/aulas/aula002/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
+    FILE *arq;
+    char c;
 
+    arq = fopen("arquivo", "r");
+
+    if (arq) {
+        while ((c = getc(arq)) != EOF) { //EOF -> END OF FILE
+            printf("%c", c);
+        }
+    } else {
+        printf("nao achei o arquivo");
+    }
     return 0;
 }
