a11aee8bdf94826858fc2d0a211a91004114bbef
vinic <viniciustegi1235@gmail.com>
2024-11-18 14:09:29 -0300

    commit new

--- curso_de_logica/ex002/main.c
+++ curso_de_logica/ex002/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+int main() {
+    float altura;
+    char sexo;
+    float peso_ideal;
+
+    printf("Digite sua altura:\n");
+    scanf("%f", &altura);
+
+    getchar();
+
+    printf("Digite o seu sexo. M para masculilo, e F para feminino:\n");
+    scanf("%c", &sexo);
+
+    if (sexo == 'M' || sexo == 'm') {
+        peso_ideal = (72.7 * altura) - 58;
+    } else if (sexo == 'F' || sexo == 'f') {
+        peso_ideal = (62.1 * altura) - 44.7;
+    } else {
+        printf("Sexo inválido. Use M ou F.\n");
+        return 1;
+    }
+    printf("Seu peso ideal eh %2.fKg", peso_ideal);
 }
