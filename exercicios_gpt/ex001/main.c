51f7fb1095c4864d1cde2dfbd87672db38980688
vinic <viniciustegi1235@gmail.com>
2024-11-19 16:33:45 -0300

    commit again

--- exercicios_gpt/ex001/main.c
+++ exercicios_gpt/ex001/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+void trocar (int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    int a;
+    int b;
+
+    printf("Type 'a' value:\n");
+        scanf("%i", &a);
+    printf("Type 'b' value:\n");
+        scanf("%i", &b);
+
+    printf("\n\nantes da troca:\n a = %d\n b = %d\n", a, b);
+
+    trocar(&a, &b);
+
+    printf("\n\ndepois da troca:\n a = %d\n b = %d", a, b);
+
+
+
     return 0;
 }
