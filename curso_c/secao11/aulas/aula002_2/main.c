ff72b46381287743ac436b2cacb8b6739f1066ab
vinic <viniciustegi1235@gmail.com>
2024-11-25 13:15:17 -0300

    commit again

--- curso_c/secao11/aulas/aula002_2/main.c
+++ curso_c/secao11/aulas/aula002_2/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    FILE *arq;
+    int num, resultado, soma = 0;
+
+    arq = fopen("arquivo.txt", "r");
+
+    if(arq) {
+        while (!feof(arq)) {
+            fscanf(arq, "%d", &num);
+            soma += num;
+        }
+    }else {
+        printf("Arquivo nao encontrado!");
+    }
+    printf("a soma dos numeros encontrados eh: %i", soma);
+    fclose(arq);
     return 0;
 }
