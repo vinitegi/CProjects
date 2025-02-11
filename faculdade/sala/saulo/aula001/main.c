b3b96ec5493975cb9689bcec07340c2e1142be1d
Vinicius Coretti Tegi <viniciustegi1235@gmail.com>
2025-02-11 19:12:30 -0300

    Commit New

---
+++ faculdade/sala/saulo/aula001/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    int m[3][3];
+    int soma = 0;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Digite o valor de m[%i][%i]:\n", i, j);
+                scanf("%i", &m[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (i == j) {
+                soma += m[i][j];
+            }
+        }
+    }
+
+    return 0;
+}
