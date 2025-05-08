2be85a60134043a499f18c5e09aea5488b4ba19b
Vinicius Tegi <viniciustegi1235@gmail.com>
2025-05-08 15:22:03 -0300

    second commit

--- faculdade/sala/diniz/p2/main.c
+++ faculdade/sala/diniz/p2/main.c
@@ -8,7 +8,7 @@ typedef struct {
     char nome[100];
     char tipo[50];
     double valor;
-}Produto;
+} Produto;
 
 int total = 0;
 
@@ -17,19 +17,15 @@ void modificarProduto(Produto item[], int total);
 void excluirProduto(Produto item[], int *total);
 void listarProdutos(Produto item[], int total); //finalizada
 void pesquisarPorId(Produto item[], int total, int id);
-void salvarProdutos(FILE *arq, Produto item[], int total); //finalizada
-void carregarProdutos(FILE *arq, Produto item[], int *total); //finalizada
+void salvarProdutos(Produto item[], int total); //finalizada
+void carregarProdutos(Produto item[], int *total); //finalizada
 
 int main(void) {
     Produto item[MAX_PRODUTOS];
     int id;
     int opcao;
 
-    FILE *arq = fopen("produtos.txt", "a+");
-    if (arq != NULL) {
-        carregarProdutos(arq, item, &total);
-        fclose(arq);
-    }
+    carregarProdutos(item, &total);
 
     do {
         printf("\n--- MENU ---\n");
@@ -38,9 +34,11 @@ int main(void) {
         printf("3. Excluir produto\n");
         printf("4. Listar produtos\n");
         printf("5. Pesquisar por ID\n");
-        printf("6. Sair\n");
-        printf("Escolha uma opção: ");
+        printf("6. Salvar produtos\n");
+        printf("7. Encerrar programa\n");
+        printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
+        getchar();
 
         switch(opcao) {
             case 1:
@@ -61,69 +59,128 @@ int main(void) {
                 pesquisarPorId(item, total, id);
                 break;
             case 6:
-                salvarProdutos(arq, item, total);
-                printf("Encerrando o programa.\n");
+                salvarProdutos(item, total);
                 break;
             default:
-                printf("Opção inválida!\n");
+                printf("Opcao inválida!\n");
+                break;
         }
-    } while(opcao != 6);
+    } while(opcao != 7);
+    printf("Encerrando o programa.\n");
 
     return 0;
 }
 
-void cadastrarProduto(Produto item[], int *total){
+void cadastrarProduto(Produto item[], int *total) {
     if (*total >= MAX_PRODUTOS) {
         printf("Numero maximo de produtos atingido");
     } else {
         printf("Informe o ID do produto: ");
-            scanf("%d", &item[*total].id);
+        scanf("%d", &item[*total].id);
         getchar();
 
         printf("Informe o nome do produto: ");
-            fgets(item[*total].nome, sizeof(item[*total].nome), stdin);
+        fgets(item[*total].nome, sizeof(item[*total].nome), stdin);
         item[*total].nome[strcspn(item[*total].nome, "\n")] = '\0';
 
         printf("Informe o tipo do produto: ");
-            fgets(item[*total].tipo, sizeof(item[*total].tipo), stdin);
+        fgets(item[*total].tipo, sizeof(item[*total].tipo), stdin);
         item[*total].tipo[strcspn(item[*total].tipo, "\n")] = '\0';
 
         printf("Informe o valor do produto: ");
-            scanf("%f", &item[*total].valor);
+        scanf("%lf", &item[*total].valor);
+        getchar();
 
         (*total)++;
     }
 }
 
-void modificarProduto(Produto item[], int total){
+void modificarProduto(Produto item[], int total) {
+    if (total == 0) {
+        printf("Nenhum produto cadastrado!\n");
+    } else {
+        int codigoBusca;
+        printf("Digite o ID do produto que sera buscado: ");
+        scanf("%i", &codigoBusca);
+        getchar();
 
+        int encontrado = 0;
+        for (int i = 0; i < total; i++) {
+            if (codigoBusca == item[i].id) {
+                printf("Digite o novo nome do produto: ");
+                fgets(item[i].nome, sizeof(item[i].nome), stdin);
+                item[i].nome[strcspn(item[i].nome, "\n")] = 0;
+
+                printf("Digite o novo tipo do produto: ");
+                fgets(item[i].tipo, sizeof(item[i].tipo), stdin);
+                item[i].tipo[strcspn(item[i].tipo, "\n")] = 0;
+
+                printf("Digite o novo valor do produto: ");
+                scanf("%lf", &item[i].valor);
+                getchar();
+
+                printf("Produto alterado com sucesso!\n");
+                encontrado = 1;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            printf("Produto com o ID %d nao encontrado!\n", codigoBusca);
+        }
+    }
 }
 
-void excluirProduto(Produto item[], int *total){
+void excluirProduto(Produto item[], int *total) {
+    if (*total == 0) {
+        printf("Nenhum produto cadastrado!\n");
+    } else {
+        int codigoBusca;
 
+        printf("Digite o ID do produto que deseja excluir: ");
+        scanf("%d", &codigoBusca);
+
+        for (int i = 0; i < *total; i++) {
+            if (item[i].id == codigoBusca) {
+                for (int j = i; j < *total - 1; j++) {
+                    item[j] = item[j + 1];
+                }
+
+                (*total)--;
+                printf("Produto excluido com sucesso\n");
+                return;
+            }
+        }
+        printf("Produto com o ID %d nao encontrado!\n", codigoBusca);
+    }
 }
 
-void listarProdutos(Produto item[], int total){
+void pesquisarPorId(Produto item[], int total, int id) {
+    if (total == 0) {
+        printf("Nenhum produto cadastrado!\n");
+    } else {
+        for (int i = 0; i < total; i++) {
+            if (id == item[i].id) {
+                printf("Produto encontrado: %s - %s - R$ %.2lf\n",
+                       item[i].nome, item[i].tipo, item[i].valor);
+                return;
+            }
+        }
+        printf("Produto com ID %d nao encontrado!\n", id);
+    }
+}
+
+void listarProdutos(Produto item[], int total) {
     if (total == 0) {
         printf("Nenhum produto cadastrado!\n");
         return;
     }
 
-    for (int i = 0; i < total; i++){
-        printf("ID: %d\n"
-               "Nome: %s\n"
-               "Tipo: %s\n"
-               "Valor: %.2f\n", item[i].id, item[i].nome, item[i].tipo, item[i].valor);
-    }
-}
-
-void pesquisarPorId(Produto item[], int total, int id){
-
-}
-
-void salvarProdutos(FILE *arq, Produto item[], int total){ //salva os produtos no arquivo
     for (int i = 0; i < total; i++) {
-        fprintf(arq, "%i;%s;%s;%f\n",
+        printf("ID: %d\n"
+            "Nome: %s\n"
+            "Tipo: %s\n"
+            "Valor: %.2f\n\n",
             item[i].id,
             item[i].nome,
             item[i].tipo,
@@ -131,13 +188,37 @@ void salvarProdutos(FILE *arq, Produto item[], int total){ //salva os produtos n
     }
 }
 
-void carregarProdutos(FILE *arq, Produto item[], int *total){ //copia os produtos salvos no arquivo para o vetor
-    while (fscanf(arq, "%d;%[^;];%[^;];%lf\n", //enquanto fscanf conseguir ler exatamente 4 itens por linha, vai continuar
+
+void salvarProdutos(Produto item[], int total) { //salva os produtos no arquivo
+    FILE *arq = fopen("produtos.txt", "w");
+    if (arq == NULL) {
+        printf("Erro ao salvar os produtos!\n");
+        return;
+    }
+
+    for (int i = 0; i < total; i++) {
+        fprintf(arq, "%i;%s;%s;%.2lf\n",
+                item[i].id,
+                item[i].nome,
+                item[i].tipo,
+                item[i].valor);
+    }
+
+    fclose(arq);
+    printf("Produtos salvos com sucesseco!\n");
+}
+
+void carregarProdutos(Produto item[], int *total) { //copia os produtos salvos no arquivo para o vetor
+    FILE *arq = fopen("produtos.txt", "r");
+    if (arq == NULL) return;
+
+    while (fscanf(arq, "%d;%[^;];%[^;];%lf\n",
                   &item[*total].id,
                   item[*total].nome,
                   item[*total].tipo,
                   &item[*total].valor) == 4) {
         (*total)++;
     }
-}
 
+    fclose(arq);
+}
